139867516cd8a71481f3c5ba7ee36b825a09777a
darshan <darshan.patel.tops@gmail.com>
2023-05-25 09:18:56 +0530

    First Commit

---
+++ PHP/02example.php
@@ -0,0 +1,17 @@
+<?php 
+
+
+
+
+
+
+$a = 420;
+$A = 100000000;
+
+echo 'hello this is darshan and he is '. $a;
+ECHO "<BR>";
+echo "hello this is darshan and he is $a and his income is $A";
+
+
+?>
+
