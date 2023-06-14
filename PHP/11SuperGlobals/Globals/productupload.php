b93d347ca8b810817e6580758d400e5f45247a9a
darshan <darshan.patel.tops@gmail.com>
2023-06-14 09:41:07 +0530

    First Commit

--- PHP/11SuperGlobals/Globals/productupload.php
+++ PHP/11SuperGlobals/Globals/productupload.php
@@ -5,5 +5,35 @@ require_once('header.php');
 
 ?>
 
+<form action="" method="post" enctype="multipart/form-data">
+<input type="text" name="price">
+<input type="file" name="image">
+<button type="submit">Submit</button>
+</form>
 
-<h1>Product upload</h1>
+
+<?php
+
+
+echo "<pre>";
+if(isset($_FILES["image"]))
+{
+    $files = $_FILES["image"];
+    // print_r($files);
+    $destination = "image/".time().$files["name"];
+    // echo $destination;
+    // echo $files['tmp_name'];
+    move_uploaded_file($files["tmp_name"],$destination);
+   $_SESSION['product_data'][] = [$_REQUEST['price'], $destination];
+    // print_r($_REQUEST);
+    // echo "session data";
+    print_r($_SESSION['product_data']);
+    echo "<br>";
+    // print_r($_FILES);
+}
+
+echo "</pre>";
+
+
+
+?>
