59d8d1c87946ef403b2f4ffa39ed356992a32ebf
darshan <darshan.patel.tops@gmail.com>
2023-09-13 09:24:23 +0530

    Update

---
+++ Laravel/Online_shopping/resources/views/home.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+<h1>Middle Page</h1>
+<h1>Middle Page</h1>
+<h1>Middle Page</h1>
+<h1>Middle Page</h1>
+<h1>Middle Page</h1>
+<h1>Middle Page</h1>
+<h1>Middle Page</h1>
+<h1>Middle Page</h1>
+<h1>Middle Page</h1>
+<h1>Middle Page</h1>
+<h1>Middle Page</h1>
+<h1>Middle Page</h1>
+<h1>Middle Page</h1>
+<h1>Middle Page</h1>
+<h1>Middle Page</h1>
+<h1>Middle Page</h1>
+<h1>Middle Page</h1>
+<h1>Middle Page</h1>
+<h1>Middle Page</h1>
+<h1>Middle Page</h1>
+<h1>Middle Page</h1>
+<h1>Middle Page</h1>
+<h1>Middle Page</h1>
+@endsection
